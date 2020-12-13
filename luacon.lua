a0c1dc5362aecd8b98117d8ab3dbd69c702eb20d
belal2453 <belal2453@gmail.com>
2020-12-13 20:28:03 +0300

    Cleaning and better error handling

--- luacon.lua
+++ luacon.lua
@@ -1,7 +1,10 @@
 local parser = require 'argparse'()
 
-local BASE_DIR    <const> = "C:/Lua/"
-local LUA_VERSIONS<const> = {
+-- You might consider changing this for a system wide compatibility
+-- this is just a dirty fix for my use case
+local CONFIGS_DIR <const> = arg[0]..[[/../luacon-configs.lua]]
+local BASE_DIR    <const> = "C:/Lua/" -- Where all of the Lua installations exists
+local LUA_VERSIONS<const> = {         -- Available Lua versions
   ["Lua 5.1"] = true,
   ["Lua 5.2"] = true,
   ["Lua 5.3"] = true,
@@ -9,55 +12,79 @@ local LUA_VERSIONS<const> = {
   ["LuaJIT"]  = true,
 }
 
-local BATCH_EXT   <const> = "cmd"
-local LUA_VERSION <const> = "Lua 5.4"
--- You might consider changing this for a system wide compatibility
--- this is just a dirty fix for my use case
-local CONFIGS_DIR <const> = arg[0]..[[/../luacon-configs.lua]]
-
-local defaultConfigs      = {
-  CURRENT_LUA_VERSION    = LUA_VERSION,
-  CURRENT_ROCKS_VERSION  = LUA_VERSION ~= "LuaJIT" and LUA_VERSION or "Lua 5.1",
-
-  DIR_BATCH_PATH         = BASE_DIR.. 'cmd/',
-  DIR_ROCKS_TREE         = BASE_DIR.. 'LuaRocks/',
-  DIR_LUA_TREE           = BASE_DIR.. LUA_VERSION,
-  NAME_LUA_INTERP        = (LUA_VERSION ~= "LuaJIT" and 'lua.exe' or 'luajit.exe'),
-  EXTENSION_BATCH        = BATCH_EXT,
+local ERR_MSGS <const> = {
+  openConfig   = "Attempt to open the config file : ",
+  loadConfig   = "Attempt to load the config file : ",
+  writeConfig  = "Attempt to write to the config file : ",
+  createConfig = 'Attempt to create the config file "%s" : ',
+  
+  openBatch  = 'Attempt to open/create the batch file "%s" : ',
+  writeBatch = "Attempt to write data to the batch file : ",
+  
+  hardcodedOpen  = "Attempt to open LuaRocks's hardcoded.lua : ",
+  hardcodedWrite = "Attempt to write data to LuaRocks's hardcoded.lua file : "
 }
 
-do -- set default configs that depends on other configs
-  local dc = defaultConfigs
-
-  dc ["DIR_LUA_BIN"]    = dc.DIR_LUA_TREE   .. '/bin/'
-  dc ["DIR_LUA_INC"]    = dc.DIR_LUA_TREE   .. '/include/'
-  dc ["DIR_LUA_LIB"]    = dc.DIR_LUA_TREE   .. '/bin/' -- On my machine, this is the only accepted dir by luarocks,
-  -- even when it is just a regular lua installalition.
-  -- you might want to change that up to Lua/lib.
-
-  dc ["DIR_ROCKS_CORE"] = dc.DIR_ROCKS_TREE .. 'lua/luarocks/core/'
-  dc ["DIR_LUA_BATCH" ] = dc.DIR_BATCH_PATH .. 'lua.' .. dc.EXTENSION_BATCH
-  dc ["COMMAND_BATCH" ] = ('@"{DIR_LUA_BIN}%s" %%*')
-    :format(dc.NAME_LUA_INTERP)
+do
+  local oAssert = assert
+  assert = function(val, ...)
+    local msgs = {...}
+    return oAssert(val, (msgs[2] or '')..msgs[1])
+  end
 end
 
------------------------------------------
+--------------------------------------------------------
+--             Handling Default Configs               --
+--------------------------------------------------------
+
+local LUA_VERSION <const> = "Lua 5.4" -- The default Lua version to use
+
+local defaultConfigs      = {
+  CURRENT_LUA_VERSION      = LUA_VERSION,
+  CURRENT_ROCKS_VERSION    = LUA_VERSION ~= "LuaJIT" and LUA_VERSION or "Lua 5.1",
+
+  DIR_BATCH_PATH           = BASE_DIR .. 'cmd/',
+  DIR_ROCKS_TREE           = BASE_DIR .. 'LuaRocks/',
+  DIR_LUA_TREE             = BASE_DIR .. LUA_VERSION,
+  NAME_LUA_INTERP          = (LUA_VERSION ~= 'LuaJIT' and 'lua.exe' or 'luajit.exe'),
+  EXTENSION_BATCH          = "cmd",
+}
+
+do -- Set default configs that depends on previous configs
+  local dc = defaultConfigs
+
+  dc["DIR_LUA_BIN"]    = dc.DIR_LUA_TREE   .. '/bin/'
+  dc["DIR_LUA_INC"]    = dc.DIR_LUA_TREE   .. '/include/'
+  
+  -- On my machine, this is the only accepted dir by luarocks,
+  -- even when it is just a regular lua installation, this seems to be a LuaRocks bug.
+  -- You might want to change that up to "Lua/lib" instead.
+  dc["DIR_LUA_LIB"]    = dc.DIR_LUA_TREE   .. '/bin/'
+
+  dc["DIR_ROCKS_CORE"] = dc.DIR_ROCKS_TREE .. 'lua/luarocks/core/'
+  dc["DIR_LUA_BATCH" ] = dc.DIR_BATCH_PATH .. 'lua.' .. dc.EXTENSION_BATCH
+  dc["COMMAND_BATCH" ] = ('@"{DIR_LUA_BIN}%s" %%*'):format(dc.NAME_LUA_INTERP)
+end
+
+--------------------------------------------------------
+--                  Handling Configs                  --
+--------------------------------------------------------
 
 local configs
 
 local function wrapAsText(t, v)
-  return (v or "[[").. t.. (v or "]]")
+  return ("%s%s%s"):format(v or "[[", t, v or "]]")
 end
 
 local function loadConfigs()
   if configs then return configs end -- No need to load again
   local data, err, success
 
-  data = assert(io.open(CONFIGS_DIR, "r")):read('a')
+  data = assert(io.open(CONFIGS_DIR, "r"), ERR_MSGS.openConfig):read('a')
   success, data, err = pcall(load(data, 'Configs'))
 
   if not success and data or not data and err then
-    parser:error("Attempt loading configs file : ".. data or err or 'Unknown issue')
+    parser:error(ERR_MSGS.loadConfig..data or err or 'Unknown error')
   end
 
   configs = data or {}
@@ -65,8 +92,8 @@ local function loadConfigs()
 end
 
 local function saveConfigs(c)
-  c = c or configs
   local chunk = {"return {\n"}
+  c = c or configs
 
   do
     local buffer = {}
@@ -75,35 +102,34 @@ local function saveConfigs(c)
     end
 
     for k, v in pairs(c) do
-      table.insert(buffer, '\t'.. (k .. ' = ' .. t(v) .. ',\n'))
+      table.insert(buffer, ('\t%s = %s,\n'):format(k, t(v)))
     end
 
     table.insert(chunk, table.concat(buffer))
-    buffer = nil
   end
 
   table.insert(chunk, '}')
   chunk = table.concat(chunk)
 
-  local f = assert(io.open(CONFIGS_DIR, "w+"), "Attempt to open the configs file.")
-  assert(f:write(chunk))
+  local f = assert(io.open(CONFIGS_DIR, "w+"), ERR_MSGS.openConfig)
+  assert(f:write(chunk), ERR_MSGS.writeConfig)
   f:close()
 end
 
 local function newVersionConfigs(ver)
   if not LUA_VERSIONS[ver] then return end
-  local c = {}
 
+  local c = {}
   for k, v in pairs(configs) do c[k] = v end
 
   c.CURRENT_LUA_VERSION = ver
-  c.NAME_LUA_INTERP= (c.CURRENT_LUA_VERSION ~= "LuaJIT" and 'lua.exe' or 'luajit.exe')
+  c.NAME_LUA_INTERP = (c.CURRENT_LUA_VERSION ~= 'LuaJIT' and 'lua.exe' or 'luajit.exe')
 
-  c.DIR_LUA_TREE   = BASE_DIR.. ver
-  c.DIR_LUA_BIN    = c.DIR_LUA_TREE.. '/bin/'
-  c.DIR_LUA_INC    = c.DIR_LUA_TREE.. '/include/'
-  c.DIR_LUA_LIB    = c.DIR_LUA_TREE.. '/bin/'
-  c.COMMAND_BATCH  = ('@"{DIR_LUA_BIN}%s" %%*'):format(c.NAME_LUA_INTERP)
+  c.DIR_LUA_TREE    = BASE_DIR       .. ver
+  c.DIR_LUA_BIN     = c.DIR_LUA_TREE .. '/bin/'
+  c.DIR_LUA_INC     = c.DIR_LUA_TREE .. '/include/'
+  c.DIR_LUA_LIB     = c.DIR_LUA_TREE .. '/bin/'
+  c.COMMAND_BATCH   = ('@"{DIR_LUA_BIN}%s" %%*'):format(c.NAME_LUA_INTERP)
 
   return c
 end
@@ -113,9 +139,10 @@ local function changeLuaVersion(ver)
   configs = newVersionConfigs(ver)
 end
 
-do -- load configs
-  if not io.open(CONFIGS_DIR, "r") then
-    io.open(CONFIGS_DIR, 'w'):close()
+
+do -- Load configs
+  if not io.open(CONFIGS_DIR, 'r') then
+    assert(io.open(CONFIGS_DIR, 'w'), ERR_MSGS.createConfig:format(CONFIGS_DIR)):close()
   end
 
   loadConfigs()
@@ -123,7 +150,7 @@ do -- load configs
   if not next(configs) then
     configs = defaultConfigs
     saveConfigs()
-  else -- writing any messing configs
+  else -- Write missing configs (if any)
     local d
     for k, v in pairs(defaultConfigs) do
       if not configs[k] then
@@ -135,7 +162,9 @@ do -- load configs
   end
 end
 
------------------------------------------
+--------------------------------------------------------
+--                     Generation                     --
+--------------------------------------------------------
 
 local function generateShellScript(ver)
   changeLuaVersion(ver or configs.CURRENT_LUA_VERSION)
@@ -145,18 +174,12 @@ end
 local function generateLuaCMD(ver)
   if ver and not LUA_VERSIONS[ver] then return end
 
-  local batchfile = assert(
-    io.open(configs.DIR_LUA_BATCH, "w+"),
-    "Attempt to open Lua batch file : ".. configs.DIR_LUA_BATCH
+  local batchfile = assert(io.open(configs.DIR_LUA_BATCH, "w+"),
+    ERR_MSGS.openBatch:format(configs.DIR_LUA_BATCH)
   )
-
-  assert(
-    batchfile:write(generateShellScript(ver)),
-    "Attempt to write data to Lua batch file : ".. configs.DIR_LUA_BATCH
-  )
+  assert(batchfile:write(generateShellScript(ver)), ERR_MSGS.writeBatch)
 
   batchfile:close() -- Should handle possible errors as well?
-
   return true
 end
 
@@ -164,39 +187,35 @@ local function generateRocksCMD(ver)
   if ver and not LUA_VERSIONS[ver] then return end
   local c = newVersionConfigs(ver or configs.CURRENT_LUA_VERSION)
 
-  local HARDCODED_PATH<const> = c.DIR_ROCKS_CORE..'hardcoded.lua'
+  local HARDCODED_PATH<const> = c.DIR_ROCKS_CORE .. 'hardcoded.lua'
 
-  local hardcodedData = assert(io.open(HARDCODED_PATH, "r")):read("a")
-  local hardcodedFile = assert(
-    io.open(HARDCODED_PATH, "w"),
-    "Attempt to open Lua LuaRocks hardcoded.lua file : ".. HARDCODED_PATH
-  )
+  local hardcodedData = assert(io.open(HARDCODED_PATH, "r"), ERR_MSGS.hardcodedOpen):read('a')
+  local hardcodedFile = assert(io.open(HARDCODED_PATH, "w"), ERR_MSGS.hardcodedOpen)
 
   local newValues = {
-    -- These are all values needs changing
-    LUA_VERSION= wrapAsText(c.CURRENT_LUA_VERSION:sub(5)),
-    LUA_INCDIR = wrapAsText(c.DIR_LUA_INC),
-    LUA_LIBDIR = wrapAsText(c.DIR_LUA_LIB),
-    LUA_BINDIR = wrapAsText(c.DIR_LUA_BIN),
+    -- These are all the values need to be changed
+    LUA_VERSION     = wrapAsText(c.CURRENT_LUA_VERSION:sub(5)),
+    LUA_INCDIR      = wrapAsText(c.DIR_LUA_INC),
+    LUA_LIBDIR      = wrapAsText(c.DIR_LUA_LIB),
+    LUA_BINDIR      = wrapAsText(c.DIR_LUA_BIN),
     LUA_INTERPRETER = wrapAsText(c.NAME_LUA_INTERP),
   }
 
-  -- Just going to replace the targeted values since the index is guarantee to exist
-  hardcodedData = hardcodedData:gsub('([a-zA-z_]+)(%s*=%s*).-(,?\n)', function(index, eq, rest)
-    return newValues[index] and (index.. eq.. newValues[index].. rest)
+  -- Just going to replace the targeted fields since the index is guaranteed(?) to exist
+  hardcodedData = hardcodedData:gsub('([a-zA-Z_]+)(%s*=%s*).-(,?\n)', function(index, eq, rest)
+    return newValues[index] and ('%s%s%s%s'):format(index, eq, newValues[index], rest)
   end)
 
-  assert(
-    hardcodedFile:write(hardcodedData),
-    "Attempt to write data to LuaRocks hardcoded.lua file : ".. HARDCODED_PATH
-  )
+  assert(hardcodedFile:write(hardcodedData), ERR_MSGS.hardcodedWrite)
 end
 
------------------------------------------
+--------------------------------------------------------
+--                    CLI Handling                    --
+--------------------------------------------------------
 
 local function is_valid_version(v)
   v = v:lower():gsub('lua', ''):gsub('%.%s*', '')
-  return ({ -- This map is only used here, so no need to define it as global
+  return ({ -- This map is only used here, so no need to store it
     ['51'] = 'Lua 5.1',
     ['52'] = 'Lua 5.2',
     ['53'] = 'Lua 5.3',
@@ -219,17 +238,14 @@ local function is_valid_target(t)
 end
 
 
-local change = parser:command('change c', 'Changes the lua version used by the targeted item')
+local change = parser:command('change c', 'Changes the Lua version used by the targeted item')
 change:argument('target',
-  "The target that should use the specified version",
-  nil, is_valid_target
+  'The target which should start using the specified Lua version', nil, is_valid_target
 )
 
 change:argument('version',
-  "The version that the target should use",
-  nil, is_valid_version
+  'The Lua version the target should use', nil, is_valid_version
 )
-
 
 change:action(function(args)
   if not args.change then os.exit() end
